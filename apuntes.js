cc5ae34a41666dc9c4871d48054d321c53381b0e
unknown <alejandrojzeballos@gmail.com>
2021-06-24 00:56:04 -0400

    {add} instalcion de angular y prerequerimientos

---
+++ apuntes.js
@@ -0,0 +1,27 @@
+/* 
+es un framework desarrollado por google en la version 11
+https://angular.io/
+tiene una buena performance.
+maneja typescript 4.1 que es javascript tipado
+SPA single page aplication
+pero tiene diferentes componentes
+como header, cabezeras, elementos extra en tags 
+vamos tener una pagina que va a tener componentes
+herramientas
+-nodejs descargamos de la pagina de nodejs ademas vamos a necesitar npm pero ya se instala con nodejs
+
+versiones de uso para este curso
+nodejs 12.18
+npm 6.14
+
+Id visual studio
+
+framework angular
+npm angular -g @angular/cli
+instalamos el cliente de angular de manera global ahora tenemos en el path el ng
+ng --version 
+vemos la version de angular 
+angularCLI 12.0
+los cambios son menores entre versiones
+
+*/
