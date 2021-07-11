f27c1f05205f76546796755956c36f4741edd542
unknown <alejandrojzeballos@gmail.com>
2021-07-11 00:48:16 -0400

    {add} añadiendo los modelos

---
+++ presupuesto-app/src/app/ingresos.model.ts
@@ -0,0 +1,8 @@
+export class Ingreso{
+    nombre:string;
+    cantidad:number;
+    constructor(nombre:string, cantidad:number){
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+    }
+}
