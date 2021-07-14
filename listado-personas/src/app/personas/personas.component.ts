7fddfd5a3382f77e69246df4bc46e185d0bce179
unknown <alejandrojzeballos@gmail.com>
2021-07-14 01:52:53 -0400

    {add} agregando routing a un componenete y redireccionando

--- listado-personas/src/app/personas/personas.component.ts
+++ listado-personas/src/app/personas/personas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { LogginService } from '../LogginService.service';
 import { Persona } from '../persona.model';
 import { PersonaService } from '../personas.services';
@@ -13,9 +14,18 @@ export class PersonasComponent implements OnInit {
   personas: Persona[] = [];
   
   constructor(private logginService: LogginService,
-              private personaService: PersonaService){}
+              private personaService: PersonaService,
+              private router:Router
+              ){}
  
   ngOnInit():void{
     this.personas = this.personaService.personas;
+  }
+
+  agregar(){
+    //inyectamos router para navegar entre componenes
+    //redireccionamos
+    this.router.navigate(['personas/agregar']);
+    //damos una url
   }
 }
