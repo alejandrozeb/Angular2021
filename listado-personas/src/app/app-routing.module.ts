e4f17d25349c3973cffabf06b8af7f82f5b34044
unknown <alejandrojzeballos@gmail.com>
2021-07-14 01:35:39 -0400

    {add} configurando el ruiting y añadiendo las rutas

--- listado-personas/src/app/app-routing.module.ts
+++ listado-personas/src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { PersonasComponent } from './personas/personas.component';
+import { FormularioComponent } from './personas/formulario/formulario.component';
 
-
+const routes: Routes=[
+  {path: '', component: PersonasComponent},//se despliega en el index principal
+  {path: 'personas', component: PersonasComponent},
+  //la / se agrega solo /personas
+  {path: 'personas/agregar', component: FormularioComponent},
+  {path: 'personas/:id', component: FormularioComponent}
+]
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(
+    routes
+  )],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
